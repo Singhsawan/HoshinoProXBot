3fa55baa20aee5660d33f1fea996910951790cfe
epic-designer <mohsinkhan1292002@gmail.com>
2023-06-24 00:53:19 +0530

    fixing cosplay

--- Hoshino/modules/cosplay.py
+++ Hoshino/modules/cosplay.py
@@ -8,26 +8,26 @@ from pyrogram import filters
 from pyrogram.types import Message,InlineKeyboardButton,InlineKeyboardMarkup
 from pyrogram.enums import *
 #NAME -> YOUR BOTS FILE NAME 
-from Hoshino import app
+from Hoshino import pbot
 
 
-@app.on_message(filters.command("cosplay"))
+@pbot.on_message(filters.command("cosplay"))
 async def cosplay(_,msg):
-    img = requests.get("https://waifu-api.vercel.app").json()
-    await msg.reply_photo(img, caption=f"Cosplay By @{app.me.username}\nCredits: @NovaXMod")
+    img = requests.get("https://waifu-api.vercel.pbot").json()
+    await msg.reply_photo(img, caption=f"Cosplay By @{pbot.me.username}\nCredits: @NovaXMod")
 
-@app.on_message(filters.command("ncosplay"))
+@pbot.on_message(filters.command("ncosplay"))
 async def ncosplay(_,msg):
     if msg.chat.type != ChatType.PRIVATE:
       await msg.reply_text("Sorry you can use this command only in private chat with bot",
         reply_markup=InlineKeyboardMarkup(
             [
-                [InlineKeyboardButton("Go PM",url=f"https://t.me/{app.me.username}?start=True")]
+                [InlineKeyboardButton("Go PM",url=f"https://t.me/{pbot.me.username}?start=True")]
             ]
         ))
     else:
-       ncosplay = requests.get("https://waifu-api.vercel.app/items/1").json()
+       ncosplay = requests.get("https://waifu-api.vercel.pbot/items/1").json()
 
-       await msg.reply_photo(ncosplay, caption=f"Cosplay By @{app.me.username}\nCredits: @NovaXMod")
+       await msg.reply_photo(ncosplay, caption=f"Cosplay By @{pbot.me.username}\nCredits: @NovaXMod")
 
 
