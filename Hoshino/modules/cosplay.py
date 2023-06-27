9075cd9a02796c7384025b115e757387de6a9ef5
epic-designer <mohsinkhan1292002@gmail.com>
2023-06-27 12:30:10 +0530

    fixed ncosplay

--- Hoshino/modules/cosplay.py
+++ Hoshino/modules/cosplay.py
@@ -16,7 +16,7 @@ from Hoshino.modules.disable import DisableAbleCommandHandler
 @pbot.on_message(filters.command("ncosplay"))
 async def ncosplay(_,msg):
     if msg.chat.type != ChatType.PRIVATE:
-      msg.reply_text("Sorry you can use this command only in private chat with bot",
+      await msg.reply_text("Sorry you can use this command only in private chat with bot",
         reply_markup=InlineKeyboardMarkup(
             [
                 [InlineKeyboardButton("Go PM",url=f"https://t.me/{pbot.me.username}?start=True")]
@@ -25,4 +25,4 @@ async def ncosplay(_,msg):
     else:
        ncosplay = requests.get("https://waifu-api.vercel.app/items/1").json()
 
-       msg.reply_photo(ncosplay, caption=f"Cosplay By @{pbot.me.username}")
+       await msg.reply_photo(ncosplay, caption=f"Cosplay By @{pbot.me.username}")
