916b72c6d711cfe1552b88ecae66f0062bc5548c
epic-designer <mohsinkhan1292002@gmail.com>
2023-06-27 14:22:10 +0530

    fixing quote

--- Hoshino/modules/quote.py
+++ Hoshino/modules/quote.py
@@ -12,27 +12,9 @@ from Hoshino import dispatcher
 url = "https://animechan.xyz/api/random"
 
 
-@run_async
-def quote(update, context):
+@pbot.on_message(filters.command("quote"))
+async def quote(update, context):
     msg = update.effective_message
-    quot = requests.get("https://waifu-api.vercel.app").json()
-    msg.reply_text(f"Anime: {quot.anime}\nCharacter: {quot.character}\nQuote: {quot.quote}")
-
-
-
-
-QUOTE_HANDLER = DisableAbleCommandHandler("quote", quote)
-dispatcher.add_handler(QUOTE_HANDLER)
-
-__handlers__ = [
-    QUOTE_HANDLER
-]
-
-__mod_name__ = "Quote"
-
-__help__ = """
-*Commands* *:*  
-
-• `/quote`*:* Get random anime quote.
-"""
+    quot = requests.get(url).json()
+    await msg.reply_text(f"Anime: {quot.anime}\nCharacter: {quot.character}\nQuote: {quot.quote}")
 
