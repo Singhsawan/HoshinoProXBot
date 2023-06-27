fd068e6986b1b63151e6252ad36d98d8029ede7b
epic-designer <mohsinkhan1292002@gmail.com>
2023-06-27 14:28:04 +0530

    fixing quote

--- Hoshino/modules/quote.py
+++ Hoshino/modules/quote.py
@@ -6,15 +6,12 @@ from pyrogram import filters
 
 from Hoshino import pbot
 
-from Hoshino.modules.disable import DisableAbleCommandHandler
-from Hoshino import dispatcher
 
 url = "https://animechan.xyz/api/random"
 
 
 @pbot.on_message(filters.command("quote"))
 async def quote(update, context):
-    msg = update.effective_message
     quot = requests.get(url).json()
-    await msg.reply_text(f"Anime: {quot.anime}\nCharacter: {quot.character}\nQuote: {quot.quote}")
+    await context.reply_text(f"Anime: {quot.anime}\nCharacter: {quot.character}\nQuote: {quot.quote}")
 
