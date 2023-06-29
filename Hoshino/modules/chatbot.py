9c2063a40c3ba3bcdf26f311bfcaee1cfd0c22c2
epic-designer <mohsinkhan1292002@gmail.com>
2023-06-29 20:49:28 +0530

    Retriving couple.py

--- Hoshino/modules/chatbot.py
+++ Hoshino/modules/chatbot.py
@@ -1,26 +1,168 @@
+import html
+import json
+import re
+from time import sleep
+
 import requests
-from pyrogram import filters, Client
-from pyrogram.types import Message
-from pyrogram.enums import ChatType
-from Hoshino import pbot
+from telegram import (
+    CallbackQuery,
+    Chat,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    ParseMode,
+    Update,
+    User,
+)
+from telegram.ext import (
+    CallbackContext,
+    CallbackQueryHandler,
+    CommandHandler,
+    Filters,
+    MessageHandler,
+)
+from telegram.utils.helpers import mention_html
 
-@pbot.on_message(~filters.bot & ~filters.me & filters.text)
-async def chatbot(_:Client,message:Message):
-    if message.chat.type!= ChatType.PRIVATE:
-        if not message.reply_to_message:
-            return
-        if message.reply_to_message.from_user.id != (await pbot.get_me()).id:
-            return
-    if message.text and message.text[0] in ["/", "!", "?", "."]:
-        return
-    response = requests.get("https://sugoi-api.vercel.app/chat?msg="+ message.text)
-    if response.status_code == 200:
-        data = response.json()['response']
-        return await message.reply_text(data)
-    elif response.status_code == 429:
-        return await message.reply_text("ChatBot Error: Too many requests. Please wait a few moments.")
-    elif response.status_code >= 500:
-        return await message.reply_text("ChatBot Error: API server error. Contact us at @tyranteyeeee.")
+import Hoshino.modules.sql.chatbot_sql as sql
+from Hoshino import BOT_ID, BOT_NAME, BOT_USERNAME, dispatcher
+from Hoshino.modules.helper_funcs.chat_status import user_admin, user_admin_no_reply
+from Hoshino.modules.log_channel import gloggable
+
+
+@user_admin_no_reply
+@gloggable
+def fallenrm(update: Update, context: CallbackContext) -> str:
+    query: Optional[CallbackQuery] = update.callback_query
+    user: Optional[User] = update.effective_user
+    match = re.match(r"rm_chat\((.+?)\)", query.data)
+    if match:
+        user_id = match.group(1)
+        chat: Optional[Chat] = update.effective_chat
+        is_fallen = sql.set_fallen(chat.id)
+        if is_fallen:
+            is_fallen = sql.set_fallen(user_id)
+            return (
+                f"<b>{html.escape(chat.title)}:</b>\n"
+                f"AI_DISABLED\n"
+                f"<b>Admin :</b> {mention_html(user.id, html.escape(user.first_name))}\n"
+            )
+        else:
+            update.effective_message.edit_text(
+                "{} ᴄʜᴀᴛʙᴏᴛ ᴅɪsᴀʙʟᴇᴅ ʙʏ {}.".format(
+                    dispatcher.bot.first_name, mention_html(user.id, user.first_name)
+                ),
+                parse_mode=ParseMode.HTML,
+            )
+
+    return ""
+
+
+@user_admin_no_reply
+@gloggable
+def fallenadd(update: Update, context: CallbackContext) -> str:
+    query: Optional[CallbackQuery] = update.callback_query
+    user: Optional[User] = update.effective_user
+    match = re.match(r"add_chat\((.+?)\)", query.data)
+    if match:
+        user_id = match.group(1)
+        chat: Optional[Chat] = update.effective_chat
+        is_fallen = sql.rem_fallen(chat.id)
+        if is_fallen:
+            is_fallen = sql.rem_fallen(user_id)
+            return (
+                f"<b>{html.escape(chat.title)}:</b>\n"
+                f"AI_ENABLE\n"
+                f"<b>Admin :</b> {mention_html(user.id, html.escape(user.first_name))}\n"
+            )
+        else:
+            update.effective_message.edit_text(
+                "{} ᴄʜᴀᴛʙᴏᴛ ᴇɴᴀʙʟᴇᴅ ʙʏ {}.".format(
+                    dispatcher.bot.first_name, mention_html(user.id, user.first_name)
+                ),
+                parse_mode=ParseMode.HTML,
+            )
+
+    return ""
+
+@user_admin
+@gloggable
+def fallen(update: Update, context: CallbackContext):
+    message = update.effective_message
+    msg = "• ᴄʜᴏᴏsᴇ ᴀɴ ᴏᴩᴛɪᴏɴ ᴛᴏ ᴇɴᴀʙʟᴇ/ᴅɪsᴀʙʟᴇ ᴄʜᴀᴛʙᴏᴛ"
+    keyboard = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(text="ᴇɴᴀʙʟᴇ", callback_data="add_chat({})"),
+                InlineKeyboardButton(text="ᴅɪsᴀʙʟᴇ", callback_data="rm_chat({})"),
+            ],
+        ]
+    )
+    message.reply_text(
+        text=msg,
+        reply_markup=keyboard,
+        parse_mode=ParseMode.HTML,
+    )
+
+
+def fallen_message(context: CallbackContext, message):
+    reply_message = message.reply_to_message
+    if message.text.lower() == "fallen":
+        return True
+    elif BOT_USERNAME in message.text.upper():
+        return True
+    elif reply_message:
+        if reply_message.from_user.id == BOT_ID:
+            return True
     else:
-        return await message.reply_text("ChatBot Error: Unknown Error Occurred. Contact us at @tyranteyeeee.")
+        return False
 
+
+def chatbot(update: Update, context: CallbackContext):
+    message = update.effective_message
+    chat_id = update.effective_chat.id
+    bot = context.bot
+    is_fallen = sql.is_fallen(chat_id)
+    if is_fallen:
+        return
+
+    if message.text and not message.document:
+        if not fallen_message(context, message):
+            return
+        bot.send_chat_action(chat_id, action="typing")
+        request = requests.get(
+            f"https://kora-api.vercel.app/chatbot/2d94e37d-937f-4d28-9196-bd5552cac68b/{BOT_NAME}/Anonymous/message={message.text}"
+        )
+        results = json.loads(request.text)
+        sleep(0.5)
+        message.reply_text(results["reply"])
+
+
+__help__ = f"""
+*{BOT_NAME} has an chatbot which provides you a seemingless chatting experience :*
+
+ »  /chatbot *:* Shows chatbot control panel
+"""
+
+__mod_name__ = "Cʜᴀᴛʙᴏᴛ"
+
+
+CHATBOTK_HANDLER = CommandHandler("chatbot", fallen, run_async=True)
+ADD_CHAT_HANDLER = CallbackQueryHandler(fallenadd, pattern=r"add_chat", run_async=True)
+RM_CHAT_HANDLER = CallbackQueryHandler(fallenrm, pattern=r"rm_chat", run_async=True)
+CHATBOT_HANDLER = MessageHandler(
+    Filters.text
+    & (~Filters.regex(r"^#[^\s]+") & ~Filters.regex(r"^!") & ~Filters.regex(r"^\/")),
+    chatbot,
+    run_async=True,
+)
+
+dispatcher.add_handler(ADD_CHAT_HANDLER)
+dispatcher.add_handler(CHATBOTK_HANDLER)
+dispatcher.add_handler(RM_CHAT_HANDLER)
+dispatcher.add_handler(CHATBOT_HANDLER)
+
+__handlers__ = [
+    ADD_CHAT_HANDLER,
+    CHATBOTK_HANDLER,
+    RM_CHAT_HANDLER,
+    CHATBOT_HANDLER,
+]
